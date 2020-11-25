767582a4845897b2edd83fc9c455329625f9fe75
Thanh Tran <trandgthanh@gmail.com>
2020-11-25 18:33:39 +0100

    social share

--- Gemfile
+++ Gemfile
@@ -37,6 +37,7 @@ gem 'simple_form'
 gem 'cloudinary', '~> 1.16.0'
 gem 'httparty', '~> 0.13.7'
 gem 'social-share-button'
+gem 'openurl', '~> 1.0'
 
 
 group :development, :test do  gem 'pry-byebug'
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+// require("social-share-button")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -32,3 +33,6 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
 });
+
+
+
